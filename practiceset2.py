4a74ed723d25ffd718a2b3256e953d9d493ea225
jalmavani027-alt <jalmavani027@gmail.com>
2026-07-15 16:53:03 +0530

    Add files via upload
    Practice set of Variables

---
+++ practiceset2.py
@@ -0,0 +1,106 @@
+#pratice set 2 (variable)
+
+a= 10
+b= 40
+
+print (a+b)
+
+c = "jal"
+d = "mavani"
+print(c+d)
+
+
+h = "35"
+k = "01"
+print(h+k)
+
+## question 2
+
+a= 34
+b= 5
+print(a%b)
+
+
+# "%" use to find the remainder of a division operation. In this case, 34 divided by 5 gives a quotient of 6 and a remainder of 4. Therefore, the output of print(a%b) will be 4.
+
+# "*" is the multiplication operator in Python. It is used to multiply two numbers together. For example, if you have a = 3 and b = 4, then a * b would give you 12.
+
+# "-" is the subtraction operator in Python. It is used to subtract one number from another. For example, if you have a = 10 and b = 4, then a - b would give you 6.
+
+# "+" is the addition operator in Python. It is used to add two numbers together. For example, if you have a = 5 and b = 3, then a + b would give you 8.
+
+# "/" is the division operator in Python. It is used to divide one number by another. For example, if you have a = 10 and b = 2, then a / b would give you 5.0.
+
+# "!=" is the not equal to operator in Python. It is used to compare two values and returns True if they are not equal, and False if they are equal. For example, if you have a = 5 and b = 3, then a != b would return True, while if a = 5 and b = 5, then a != b would return False.
+
+g = 10
+o = 5 
+u = 50
+w = 90
+
+print("the first answer is",g+o , "the second answer is", u-w)
+
+
+#question 3
+
+a = 3
+b = type(a)
+print(b)
+
+f = 4.90
+k = str(f)
+r = type(k)
+
+
+print(r)
+
+a = 1
+b = float(a)
+c = type(b)
+print(c)
+
+# Q 3 by using input function
+
+a = input("Enter a number: ")
+b = input("Enter second number: ")
+print(a+b)
+
+v = int(input("Enter a number: "))
+p = int(input("Enter second number: "))
+print(v+p)
+
+f = (input("Enter second number: "))
+k = int(f)
+print(type(k))
+
+f = type(input("Enter second number: "))
+print(f)
+
+# Q 4
+
+a = int(input("Enter a number: "))
+b = int(input("Enter a number: "))
+
+print("Is a is greater than b :" , a>b)
+
+# Q 5 average finding
+
+a = int(input("Enter a number: "))
+b = int(input("Enter a number: "))
+c = (a + b) / 2
+print("The average of", a, "and", b, "is:", c)
+
+
+
+v = int(input("Enter a number: "))
+h = int(input("Enter a number: "))
+
+print((v+h)/2)
+
+# Q 6 squaring 
+
+a = int(input("Enter a number: "))
+b = int(input("Enter a number: "))
+c = (a**2 + b**2)
+print("The square of the number is", c)
+ 
