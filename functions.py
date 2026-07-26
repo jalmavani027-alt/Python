ec588e7424cfd8406aac283b639c885efd7e3fce
Jal Mavani <jalmavani027@gmail.com>
2026-07-26 15:55:04 +0530

    Add files via upload
    Introduction to functions

---
+++ functions.py
@@ -0,0 +1,35 @@
+# a = int(input("Enter number here : "))
+# b = int(input("Enter number here : "))
+# c = int(input("Enter number here : "))
+
+# print((a+b+c)/3)
+
+# By using functions it becomes easily when to much data there
+
+# Function Definition
+def avg():
+    a = int(input("Enter number here : "))
+    b = int(input("Enter number here : "))
+    c = int(input("Enter number here : "))
+
+    print((a+b+c)/3)
+
+avg()    # Function Call
+print("Thank you!")
+# avg()
+# avg()
+# avg()
+# avg()
+
+def user():
+    a1 = input("Enter Your Name Here : ")
+    print("Good Day" ,a1)
+
+
+user()
+user()
+user()
+
+
+
+
